3055011858283382721cf0fc33184cc07bd5aeb4
Rafael <rafael.quelho@gmail.com>
2022-06-21 20:31:04 -0300

    feat: creating todo struct

--- lib/todo_hexagonal/domain/todo.ex
+++ lib/todo_hexagonal/domain/todo.ex
@@ -1,29 +1,29 @@
 defmodule TodoHexagonal.Domain.Todo do
   @type t :: %__MODULE__{
           description: String.t(),
-          datetime: DateTime.t(),
+          due_date: DateTime.t(),
           done?: boolean()
         }
 
-  defstruct description: "", datetime: nil, done?: false
+  defstruct description: "", due_date: nil, done?: false
 
   @spec create(String.t(), DateTime.t()) :: {:ok, t()} | {:error, String.t()}
-  def create(description, datetime) do
-    case todo_valid?(description, datetime) do
-      {true} -> {:ok, %__MODULE__{description: description, datetime: datetime}}
+  def create(description, due_date) do
+    case todo_valid?(description, due_date) do
+      {true} -> {:ok, %__MODULE__{description: description, due_date: due_date}}
       {false, error} -> {:error, error}
     end
   end
 
-  defp todo_valid?(_description = "", _datetime) do
+  defp todo_valid?(_description = "", _due_date) do
     {false, "A description is required"}
   end
 
-  defp todo_valid?(_description, %DateTime{} = _datetime) do
+  defp todo_valid?(_description, %DateTime{} = _due_date) do
     {true}
   end
 
-  defp todo_valid?(_description, _datetime) do
+  defp todo_valid?(_description, _due_date) do
     {false, "A datetime must be provided"}
   end
 end
