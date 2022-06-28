eadbac2f0cc7ac06c94147f922c020db4696a3ab
Rafael <rafael.quelho@gmail.com>
2022-06-28 10:53:30 -0300

    returning a domain todo after inserting in the db

--- lib/todo_hexagonal/repository/ecto_todo.ex
+++ lib/todo_hexagonal/repository/ecto_todo.ex
@@ -4,13 +4,22 @@ defmodule TodoHexagonal.Repository.EctoTodo do
   alias TodoHexagonal.Domain
   alias TodoHexagonal.EctoModel
   alias TodoHexagonal.Repo
+  alias TodoHexagonal.Translators
 
   @impl true
   def insert(description, due_date) do
     {:ok, todo} = Domain.Todo.create(description, due_date)
 
-    %EctoModel.Todo{}
-    |> EctoModel.Todo.changeset(%{description: todo.description, due_date: todo.due_date})
-    |> Repo.insert()
+    relation_map = %{description: :description, due_date: :due_date}
+
+    case %EctoModel.Todo{}
+         |> EctoModel.Todo.changeset(%{description: todo.description, due_date: todo.due_date})
+         |> Repo.insert() do
+      {:ok, ecto_todo} ->
+        {:ok, Translators.Generic.translate(ecto_todo, Domain.Todo, relation_map)}
+
+      {:error, _} = err ->
+        err
+    end
   end
 end
--- test/todo_hexagonal/repository/ecto_todo_test.exs
+++ test/todo_hexagonal/repository/ecto_todo_test.exs
@@ -2,6 +2,7 @@ defmodule TodoHexagonal.Repository.EctoTodoTest do
   use ExUnit.Case, async: true
   use TodoHexagonal.DataCase
 
+  alias TodoHexagonal.Domain
   alias TodoHexagonal.Repository.EctoTodo
 
   describe "Insert/2" do
@@ -9,7 +10,9 @@ defmodule TodoHexagonal.Repository.EctoTodoTest do
       description = "must by potatoes"
       due_date = DateTime.from_naive!(~N[2018-11-15 10:00:00], "Etc/UTC")
 
-      assert {:ok, todo} = EctoTodo.insert(description, due_date)
+      assert {:ok, todo = %Domain.Todo{}} = EctoTodo.insert(description, due_date)
+      assert todo.description == description
+      assert todo.due_date == due_date
     end
   end
 end
